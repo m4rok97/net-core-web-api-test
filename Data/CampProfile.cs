294f0da9b98c146bcac066c59b3b0516b4e6c3b3
Marcos <m4rok97@gmail.com>
2021-02-16 22:39:38 -0500

    First commit in Visual Stdio

---
+++ Data/CampProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+                .ReverseMap();
+            
+            CreateMap<Speaker, SpeakerModel>().ReverseMap();
+
+            CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Speaker, o => o.Ignore())
+                .ForMember(t => t.Camp, o => o.Ignore());
+        }
+    }
+}
